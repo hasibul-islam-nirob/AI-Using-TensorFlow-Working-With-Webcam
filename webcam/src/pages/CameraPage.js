a7fbcbe0d05641a7c3e6fa84c5f47a57430ea3ac
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-10-25 01:08:40 +0600

    Setup Environment

---
+++ webcam/src/pages/CameraPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CameraPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default CameraPage;
