cb460c27bb580078e86a507966723fa27f9baa84
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-10-25 03:40:10 +0600

    Import WebCam

--- webcam/src/components/Camera.js
+++ webcam/src/components/Camera.js
@@ -4,14 +4,29 @@ import '../assets/css/bootstrap.min.css';
 import '../assets/css/camera.css';
 import defaultImg from "../assets/img/defaultImg.png";
 import {HiOutlineSave, MdOutlineCameraAlt} from "react-icons/all";
+import Webcam from "react-webcam";
+
+
 class Camera extends Component {
+
+    constructor() {
+        super();
+        this.cameraRef = React.createRef();
+    }
+
+
     render() {
         return (
             <Fragment>
                 <Container className="">
                     <Row className="pt-5 shadow-sm bg-white" >
                         <Col className=" p-3 mb-2" sm={12} md={6} lg={6} >
-                            <img className="captureUserImg w-100" src={defaultImg} alt=""/>
+                            <Webcam
+                                audio={false}
+                                className="w-100"
+                                screenshotFormat="image/jpeg"
+                                ref={this.cameraRef}
+                            />
                             <button className="btn btn-success captureButton" > <MdOutlineCameraAlt/> Capture</button>
                         </Col>
                         <Col className=" p-3 mb-2" sm={12} md={6} lg={6} >
