f9e963d3adfea486b6c8adfdf4c58dfb2343896d
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2021-10-25 12:10:45 +0600

    Complete Webcam Project

--- webcam/src/components/Camera.js
+++ webcam/src/components/Camera.js
@@ -5,6 +5,7 @@ import '../assets/css/camera.css';
 import defaultImg from "../assets/img/defaultImg.png";
 import {HiOutlineSave, MdOutlineCameraAlt} from "react-icons/all";
 import Webcam from "react-webcam";
+import SweetAlert from "react-bootstrap-sweetalert";
 
 
 class Camera extends Component {
@@ -14,14 +15,40 @@ class Camera extends Component {
         this.cameraRef = React.createRef();
 
         this.state={
-            onCaptureImage:defaultImg
+            onCaptureImage:defaultImg,
+            userCameraError:false
         }
+    }
+
+    whenCameraError=()=>{
+        this.setState({userCameraError:true})
     }
 
     onClickCapture = ()=>{
         let captureUserImgBase64 = this.cameraRef.current.getScreenshot();
         this.setState({onCaptureImage:captureUserImgBase64});
 
+    }
+
+    onSaveImage = ()=>{
+        let imgBase64String = this.state.onCaptureImage;
+        let a = document.createElement('a');
+        a.href=imgBase64String;
+        a.download = 'webcam-image.jpeg';
+        a.click();
+    }
+
+    CameraErrorAlert=()=>{
+        if (this.state.userCameraError===true){
+            return(
+                <SweetAlert danger title="Device Camera Disable..!" onConfirm={this.tryAgain} >
+                    Check Device Camera
+                </SweetAlert>
+            )
+        }
+    }
+    tryAgain=()=>{
+        window.location.href='/';
     }
 
     render() {
@@ -31,6 +58,7 @@ class Camera extends Component {
                     <Row className="pt-5 shadow-sm bg-white" >
                         <Col className=" p-3 mb-2" sm={12} md={6} lg={6} >
                             <Webcam
+                                onUserMediaError={this.whenCameraError}
                                 audio={false}
                                 className="w-100"
                                 screenshotFormat="image/jpeg"
@@ -40,10 +68,11 @@ class Camera extends Component {
                         </Col>
                         <Col className=" p-3 mb-2" sm={12} md={6} lg={6} >
                             <img className="saveUserImg  w-100" src={this.state.onCaptureImage} alt=""/>
-                            <button className="btn btn-success saveButton " > <HiOutlineSave/> Save </button>
+                            <button onClick={this.onSaveImage} className="btn btn-success saveButton " > <HiOutlineSave/> Save </button>
                         </Col>
                     </Row>
                 </Container>
+                {this.CameraErrorAlert()}
             </Fragment>
         );
     }
