4a53e78123e71583311029996ec70737fde294f4
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2022-02-25 16:34:24 +0600

    Smart Camera Project Done

--- Smart-Camera/smart-cam/src/components/Camera.js
+++ Smart-Camera/smart-cam/src/components/Camera.js
@@ -1,5 +1,5 @@
 import React, {Component, Fragment} from 'react';
-import {Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Row,Button} from "react-bootstrap";
 import '../assets/css/bootstrap.min.css';
 import '../assets/css/camera.css';
 import defaultImg from "../assets/img/defaultImg.png";
@@ -27,7 +27,12 @@ class Camera extends Component {
             gender:"",
             expression:"",
             faceExpression:[],
-            faceLandmarkPoint:[]
+            faceLandmarkPoint:[],
+            left_to_Right_Eye:0.0,
+            nose_to_Left_Mouth:0.0,
+            nose_to_Right_Mouth:0.0,
+            nose_to_Right_Eye:0.0,
+            nose_to_Left_Eye:0.0,
         }
     }
 
@@ -40,6 +45,7 @@ class Camera extends Component {
         this.ageAndGenderDetection()
         this.FaceExpression()
         this.FaceLandMark()
+
     }
     onSaveImage = ()=>{
         let imgBase64String = this.state.onCaptureImage;
@@ -121,8 +127,56 @@ class Camera extends Component {
             await faceAPI.nets.faceLandmark68Net.loadFromUri('models/');
             const result = await faceAPI.detectAllFaces(Img).withFaceLandmarks();
             this.setState({faceLandmarkPoint:result});
+
             this.setState({loaderDiv:"d-none"})
         })()
+    }
+
+
+    Left_to_Right_Eye=()=>{
+        let faceLandmarkPoint =  this.state.faceLandmarkPoint;
+        let x1=  faceLandmarkPoint[0]['landmarks']['_positions'][37]['_x']
+        let y1=  faceLandmarkPoint[0]['landmarks']['_positions'][37]['_y']
+        let x2=  faceLandmarkPoint[0]['landmarks']['_positions'][46]['_x']
+        let y2=  faceLandmarkPoint[0]['landmarks']['_positions'][46]['_y']
+        let dist=Math.sqrt( (Math.pow((x1-x2),2))+(Math.pow((y1-y2),2)));
+        this.setState({left_to_Right_Eye:dist})
+    }
+    Nose_to_Left_Mouth=()=>{
+        let faceLandmarkPoint =  this.state.faceLandmarkPoint;
+        let x1=  faceLandmarkPoint[0]['landmarks']['_positions'][31]['_x']
+        let y1=  faceLandmarkPoint[0]['landmarks']['_positions'][31]['_y']
+        let x2=  faceLandmarkPoint[0]['landmarks']['_positions'][49]['_x']
+        let y2=  faceLandmarkPoint[0]['landmarks']['_positions'][49]['_y']
+        let dist=Math.sqrt( (Math.pow((x1-x2),2))+(Math.pow((y1-y2),2)));
+        this.setState({nose_to_Left_Mouth:dist})
+    }
+    Nose_to_Right_Mouth=()=>{
+        let faceLandmarkPoint =  this.state.faceLandmarkPoint;
+        let x1=  faceLandmarkPoint[0]['landmarks']['_positions'][31]['_x']
+        let y1=  faceLandmarkPoint[0]['landmarks']['_positions'][31]['_y']
+        let x2=  faceLandmarkPoint[0]['landmarks']['_positions'][55]['_x']
+        let y2=  faceLandmarkPoint[0]['landmarks']['_positions'][55]['_y']
+        let dist=Math.sqrt( (Math.pow((x1-x2),2))+(Math.pow((y1-y2),2)));
+        this.setState({nose_to_Right_Mouth:dist})
+    }
+    Nose_to_Right_Eye=()=> {
+        let faceLandmarkPoint =  this.state.faceLandmarkPoint;
+        let x1 = faceLandmarkPoint[0]['landmarks']['_positions'][31]['_x']
+        let y1 = faceLandmarkPoint[0]['landmarks']['_positions'][31]['_y']
+        let x2 = faceLandmarkPoint[0]['landmarks']['_positions'][46]['_x']
+        let y2 = faceLandmarkPoint[0]['landmarks']['_positions'][46]['_y']
+        let dist = Math.sqrt((Math.pow((x1 - x2), 2)) + (Math.pow((y1 - y2), 2)));
+        this.setState({nose_to_Right_Eye: dist})
+    }
+    Nose_to_Left_Eye=()=>{
+        let faceLandmarkPoint =  this.state.faceLandmarkPoint;
+        let x1=  faceLandmarkPoint[0]['landmarks']['_positions'][31]['_x']
+        let y1=  faceLandmarkPoint[0]['landmarks']['_positions'][31]['_y']
+        let x2=  faceLandmarkPoint[0]['landmarks']['_positions'][37]['_x']
+        let y2=  faceLandmarkPoint[0]['landmarks']['_positions'][37]['_y']
+        let dist=Math.sqrt( (Math.pow((x1-x2),2))+(Math.pow((y1-y2),2)));
+        this.setState({nose_to_Left_Eye:dist})
     }
 
     render() {
@@ -131,7 +185,7 @@ class Camera extends Component {
                 <div className={this.state.mainDiv}>
                     <Container className="">
                         <Row className="pt-5 shadow-sm bg-white" >
-                            <Col className=" p-3 mb-2" sm={12} md={4} lg={4} >
+                            <Col className=" p-3 mb-2" sm={12} md={6} lg={6} >
                                 <Webcam
                                     onUserMediaError={this.whenCameraError}
                                     audio={false}
@@ -141,9 +195,9 @@ class Camera extends Component {
                                 />
                                 <button onClick={this.onClickCapture} className="btn btn-success captureButton" > <MdOutlineCameraAlt/> Capture</button>
                             </Col>
-                            <Col className=" p-3 mb-2" sm={12} md={4} lg={4} >
+                            <Col className=" p-3 mb-2" sm={12} md={6} lg={6} >
                                 <img id="img" className="saveUserImg  w-100" src={this.state.onCaptureImage} alt=""/>
-                                <button onClick={this.onSaveImage} className="btn btn-success saveButton " > <HiOutlineSave/> Save </button>
+                                {/*<button onClick={this.onSaveImage} className="btn btn-success saveButton " > <HiOutlineSave/> Save </button>*/}
                             </Col>
 
                             <Col className=" p-3 mb-2" sm={12} md={4} lg={4} >
@@ -153,6 +207,45 @@ class Camera extends Component {
                             </Col>
                         </Row>
                     </Container>
+
+
+                    <Container fluid={true}>
+                        <Row  className=" m-2  p-5 ">
+                            <Col>
+                                <table className="table ">
+                                    <thead>
+                                    <tr>
+                                        <th className="text-danger">Age</th>
+                                        <th className="text-success">{this.state.age}</th>
+                                        <th className="text-danger">Gender</th>
+                                        <th className="text-success">{this.state.gender}</th>
+                                        <th className="text-danger">Expression</th>
+                                        <th className="text-success">{this.state.expression}</th>
+                                        <th> <Button onClick={this.Nose_to_Left_Eye}  className="btn btn-info">Nose_to_Left_Eye</Button> </th>
+                                        <th className="text-success">{(this.state.nose_to_Left_Eye).toFixed(2)}</th>
+                                    </tr>
+
+                                    <tr>
+                                        <th> <Button onClick={this.Left_to_Right_Eye}  className="btn btn-info">Left_to_Right_Eye</Button> </th>
+                                        <th className="text-success">{(this.state.left_to_Right_Eye).toFixed(2)}</th>
+                                        <th> <Button onClick={this.Nose_to_Left_Mouth}  className="btn btn-info">Nose_to_Left_Mouth</Button> </th>
+                                        <th className="text-success">{(this.state.nose_to_Left_Mouth).toFixed(2)}</th>
+                                        <th> <Button onClick={this.Nose_to_Right_Mouth}  className="btn btn-info">Nose_to_Right_Mo
+                                            uth</Button> </th>
+                                        <th className="text-success">{(this.state.nose_to_Right_Mouth).toFixed(2)}</th>
+                                        <th> <Button onClick={this.Nose_to_Right_Eye}  className="btn btn-info">Nose_to_Right_Eye</Button> </th>
+                                        <th className="text-success">{(this.state.nose_to_Right_Eye).toFixed(2)}</th>
+                                    </tr>
+
+                                    </thead>
+                                    <tbody>
+                                    </tbody>
+                                </table>
+                            </Col>
+                        </Row>
+                    </Container>
+
+
                     <Container className="mb-5">
                         <Row>
                             <Col className="my-2" sm={12} md={4} lg={4} >
